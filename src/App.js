b6f7ef5eaa6c2aa3ba639cbb44d3d97516c19304
Hyacinthia M. Groeizaam <cinnymaria@gmail.com>
2021-06-04 15:02:55 +0200

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./menu-bar/App.css";
 import Navigation from "./menu-bar/pages/Navigation";
-import Home from "./menu-bar/pages/Home";
 import About from "./menu-bar/pages/About";
 import Contact from "./menu-bar/pages/Contact";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-      <Router>
+    <Router>
+      <div className="App">
         <Navigation />
-        <Route path="/" conponent={Home}/>
-        <Route path="/About" component={About} />
-        <Route path="/Contact" component={Contact} />
-      </Router>
-    </div>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/About" component={About} />
+          <Route path="/Contact" component={Contact} />
+        </Switch>
+      </div>
+    </Router>
   );
+};
+
+const Home = () => {
+  return(
+    <div><h1>Home</h1></div>
+  )
 };
 
 export default App;
--- src/menu-bar/pages/Navigation.js
+++ src/menu-bar/pages/Navigation.js
@@ -1,18 +1,28 @@
-import React from 'react';
-import './Navigation.css';
+import React from "react";
+import "./Navigation.css";
+import { Link } from "react-router-dom";
 
 
-const Navigation=()=> {
-    return (
-        <nav>
-            <h3>logo</h3>
-            <ul className='navLinks'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-        </nav>
-    )
-}
+const Navigation = () => {
+    const navStyle = {
+        color: 'white'
+    }
+  return (
+    <nav>
+      <h3>logo</h3>
+      <ul className="navLinks">
+        <Link style={navStyle} to='/'>
+          <li>Home</li>
+        </Link>
+        <Link style={navStyle} to='/About'>
+          <li>About</li>
+        </Link>
+        <Link style={navStyle} to='Contact'>
+          <li>Contact</li>
+        </Link>
+      </ul>
+    </nav>
+  );
+};
 
-export default Navigation
+export default Navigation;
